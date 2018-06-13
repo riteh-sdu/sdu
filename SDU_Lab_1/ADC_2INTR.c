782740ce5424065b1d05f3e6ebc57c0ff78d3a6f
SDUDorMihBor <39735486+SDUDorMihBor@users.noreply.github.com>
2018-06-13 13:51:49 +0200

    Add files via upload
    Finalna verzija programa

--- SDU_Lab_1/ADC_2INTR.c
+++ SDU_Lab_1/ADC_2INTR.c
@@ -27,9 +27,7 @@ unsigned long Vfb_Bus_offset;
 void Setup_ePWM(void);
 interrupt void int_rut(void);
 
-//naš dio interupt 2
-
-_iq Vfb_Bus_offs = _IQ(257);
+// Dio interupt_2
 
 _iq TH_HV = _IQ(2000);										// granicna vrijednost za visoki napon
 _iq TH_LV = _IQ(1000);										// granicna vrijednost za niski napon
@@ -53,43 +51,45 @@ _iq16 zbroj_I_U = _IQ(0);									// brojnik za MV filtar struje faze U
 _iq16 zbroj_I_V = _IQ(0);									// brojnik za MV filtar struje faze V
 
 int i_VN = 0, i_NN = 0; 									// brojaèi za napone (visoki i niski)
-int i_mz = 0, i_vz = 0;										// brojaci za struje (mali i veliki zateg)
+int i_mz_pov = 0, i_mz_U = 0, i_mz_V = 0;					// brojaci za struje (mali zateg)
+int i_vz_pov = 0, i_vz_U = 0, i_vz_V = 0;					// brojaci za struje (veliki zateg)
 
 int T_mz = 20, T_vz = 1000;									// vrijeme za strju sa malim i velikim zategom
-// treba dobiti od ADC jedinice ??????
-long int vrijeme_VN = 1000, vrijeme_NN = 100;				// vrijeme za visoki napon i niski napon
+long int vrijeme_VN = 100, vrijeme_NN = 1000;				// vrijeme za visoki napon i niski napon
 
 int test_VN = 0, test_MN = 0;								// detekcija visokog i niskog napona
-int test_Imz_U = 0; test_Imz_V = 0; test_Imz_W = 0;			// detekcija struje sa malim zategom
-int test_Ivz_U = 0; test_Ivz_V = 0; test_Ivz_W = 0;			// detekcija struja sa velikim zategom
+int test_Imz_U = 0, test_Imz_V = 0, test_Imz_pov = 0;		// detekcija struje sa malim zategom
+int test_Ivz_U = 0, test_Ivz_V = 0, test_Ivz_pov = 0;		// detekcija struja sa velikim zategom
 
 
 void Interupt_2(void){
+	// Bufferi 							------------------------------------------------------
+
 	// Buffer za napon
 	Buffer_Vfb[3] = Buffer_Vfb[2];
 	Buffer_Vfb[2] = Buffer_Vfb[1];
 	Buffer_Vfb[1] = Buffer_Vfb[0];
-	Buffer_Vfb[0] =  _IQ(Vfb_Bus)- Vfb_Bus_offs;
+	Buffer_Vfb[0] =  _IQ(Vfb_Bus) - _IQ(Vfb_Bus_offset);
 
 	// buffer za povratnu struju
 	Buffer_I_pov[3] = Buffer_I_pov[2];
 	Buffer_I_pov[2] = Buffer_I_pov[1];
 	Buffer_I_pov[1] = Buffer_I_pov[0];
-	Buffer_I_pov[0] =  _IQ(Ifb_Ret-Ifb_Ret_offset);
+	Buffer_I_pov[0] =  _IQ(Ifb_Ret) - _IQ(Ifb_Ret_offset);
 
 	// buffer za struju faze U
 	Buffer_I_U[3] = Buffer_I_U[2];
 	Buffer_I_U[2] = Buffer_I_U[1];
 	Buffer_I_U[1] = Buffer_I_U[0];
-	Buffer_I_U[0] =  _IQ(Ifb_U-Ifb_U_offset);
+	Buffer_I_U[0] =  _IQ(Ifb_U) - _IQ(Ifb_U_offset);
 
 	// buffer za struju faze V
 	Buffer_I_V[3] = Buffer_I_V[2];
 	Buffer_I_V[2] = Buffer_I_V[1];
 	Buffer_I_V[1] = Buffer_I_V[0];
-	Buffer_I_V[0] =  _IQ(Ifb_V-Ifb_V_offset);
+	Buffer_I_V[0] =  _IQ(Ifb_V) - _IQ(Ifb_V_offset);
 
-	// Radimo Moving average filter
+	// Radimo Moving average filter 	------------------------------------------------------
 
 	// za napon DC linka
 	zbroj_Vfb = _IQtoIQ16(Buffer_Vfb[3]) + _IQtoIQ16(Buffer_Vfb[2]) + _IQtoIQ16(Buffer_Vfb[1]) + _IQtoIQ16(Buffer_Vfb[0]);
@@ -108,53 +108,159 @@ void Interupt_2(void){
 	MV_struja_V = _IQ16toIQ(_IQmpy(zbroj_I_V, N));
 
 	// minimalna vrijednost napona DC linka
-	if (MV_napon < Vfb_Bus_offs){
-		MV_napon = _IQ(0);								// pitati profesora za vrijednost ispod offseta
+	if (MV_napon < 0){
+		MV_napon = _IQmpy(MV_napon,_IQ(-1));
 	}
 	// minimalna vrijednost povratne struje
-	if (MV_struja_I_pov < _IQ(Ifb_Ret_offset)){
-		MV_struja_I_pov = _IQ(0);
+	if (MV_struja_I_pov < 0){
+		MV_struja_I_pov = _IQmpy(MV_struja_I_pov,_IQ(-1));
 	}
 	// minimalna vrijednost struje faze U
-	if (MV_struja_U < _IQ(Ifb_U_offset)){
-		MV_struja_U = _IQ(0);
+	if (MV_struja_U < 0){
+		MV_struja_U = _IQmpy(MV_struja_U,_IQ(-1));
 	}
 	// minimalna vrijednost struje faze V
-	if (MV_struja_V < _IQ(Ifb_V_offset)){
-		MV_struja_V = _IQ(0);
+	if (MV_struja_V < 0){
+		MV_struja_V = _IQmpy(MV_struja_V,_IQ(-1));
 	}
 
-	// Prenaponska zastita, moramo odrediti graniènu vrijednost (TH)????? dobiti od ADC jedinice ??????
+	// Prenaponska zastita 		------------------------------------------------------
 	if (MV_napon <= TH_HV){
 		test_VN = 0;
 		if (i_VN > 0) {
 			i_VN -=1;
+			if (i_VN < 0){
+				i_VN = 0;
+			}
 		}
 	}
 	else{
 		i_VN += 1;
 	}
-	if (i_VN >= vrijeme_VN) {
-		// signaliziraj da je napon veci od gornje granice
-		test_VN = 1;
+	if (i_VN >= vrijeme_VN){
+		test_VN = 1;			// signaliziraj da je napon veci od gornje granice
 	}
 
-	// Podnaponska zastita
+	// Podnaponska zastita 		------------------------------------------------------
 	if (MV_napon > TH_LV){
 		test_MN = 0;
 		if (i_NN > 0){
 			i_NN -= 1;
+			if (i_NN < 0){
+				i_NN = 0;
+			}
 		}
 	}
 	else{
 		i_NN += 1;
 	}
 	if (i_NN > vrijeme_VN){
-		// signaliziraj da je napon manji od donje granice
-		test_MN = 1;
+		test_MN = 1;			// signaliziraj da je napon manji od donje granice
 	}
 
-	// NASTAVAK SA STRUJOM
+	// Strujna zastita mali zateg ------------------------------------------------------
+
+	// Povratna struja
+	if (MV_struja_I_pov < TH_mzI){
+		test_Imz_pov = 0;
+		if (i_mz_pov > 0){
+			i_mz_pov -= 1;
+			if (i_mz_pov < 0){
+				i_mz_pov = 0;
+			}
+		}
+	}
+	else{
+		i_mz_pov += 1;
+	}
+	if (i_mz_pov >= TH_mzI){
+		test_Imz_pov = 1;		// signaliziraj da je struja veæa od granice za mali zateg
+	}
+	// Struja faze U
+	if (MV_struja_U < TH_mzI){
+		test_Imz_U = 0;
+		if (i_mz_U > 0){
+			i_mz_U -= 1;
+			if (i_mz_U < 0){
+				i_mz_U = 0;
+			}
+		}
+	}
+	else{
+		i_mz_U += 1;
+	}
+	if (i_mz_U >= TH_mzI){
+		test_Imz_U = 1;			// signaliziraj da je struja veæa od granice za mali zateg
+	}
+	// Struja faze V
+	if (MV_struja_V < TH_mzI){
+		test_Imz_V = 0;
+		if (i_mz_V > 0){
+			i_mz_V -= 1;
+			if (i_mz_V < 0){
+				i_mz_V = 0;
+			}
+		}
+	}
+	else{
+		i_mz_V += 1;
+	}
+	if (i_mz_V >= TH_mzI) {
+		test_Imz_V = 1;			// signaliziraj da je struja veæa od granice za mali zateg
+	}
+
+	// Strujna zastita veliki zateg ------------------------------------------------------
+
+	// Povratna struja
+	if (MV_struja_I_pov < TH_vzI){
+		test_Ivz_pov = 0;
+		if (i_vz_pov > 0){
+			i_vz_pov -= 1;
+			if (i_vz_pov < 0){
+				i_vz_pov = 0;
+			}
+		}
+	}
+	else{
+		i_vz_pov += 1;
+	}
+	if (i_vz_pov >= T_mz) {
+		test_Ivz_pov = 1;		// signaliziraj da je struja veæa od granice za veliki zateg
+	}
+
+	// Struja faze U
+	if (MV_struja_U < TH_vzI){
+		test_Ivz_U = 0;
+		if (i_vz_U > 0){
+			i_vz_U -= 1;
+			if (i_vz_U < 0){
+				i_vz_U = 0;
+			}
+		}
+	}
+	else{
+		i_vz_U += 1;
+	}
+	if (i_vz_U >= T_mz) {
+		test_Ivz_U = 1;			// signaliziraj da je struja veæa od granice za veliki zateg
+	}
+
+	// Struja faze U
+	if (MV_struja_V < TH_vzI){
+		test_Ivz_V = 0;
+		if (i_vz_V > 0){
+			i_vz_V -= 1;
+			if (i_vz_V < 0){
+				i_vz_V = 0;
+			}
+		}
+	}
+	else{
+		i_vz_V += 1;
+	}
+	if (i_vz_V >= T_mz) {
+		test_Ivz_V = 1;			// signaliziraj da je struja veæa od granice za veliki zateg
+	}
 
 
 }
